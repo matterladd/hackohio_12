53c97b6acfe079887fba0724c87e48764017409f
joshmarek22 <josh.marek22@gmail.com>
2024-10-19 14:04:28 -0400

    google authentication login

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,9 +1,11 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 
 import Home from '../views/Home.vue'
+import Test from '../views/Test.vue'
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/test', component: Test },
 ]
 
 const router = createRouter({
