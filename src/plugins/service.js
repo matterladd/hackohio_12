c978257e3c5e2ddd4e77590a119af07a11a23889
matterladd <matterlad@protonmail.com>
2024-10-19 16:10:10 -0400

    Implement API

--- src/plugins/service.js
+++ src/plugins/service.js
@@ -1,32 +1,19 @@
-// async function getData() {
-//     const url = "https://example.org/products.json";
-//     try {
-//       const response = await fetch(url);
-//       if (!response.ok) {
-//         throw new Error(`Response status: ${response.status}`);
-//       }
-  
-//       const json = await response.json();
-//       console.log(json);
-//     } catch (error) {
-//       console.error(error.message);
-//     }
-//   }
+import dotenv from 'dotenv';
+dotenv.config();
 
-// getData();
+const { VITE_API_ACCESS_TOKEN } = process.env;
 
-const fetch = import('node-fetch');
 
-const url = 'https://api.themoviedb.org/3/tv/series_id/season/season_number/episode/episode_number?language=en-US';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjY4OTQ1NDhjMTY3NjJkYTAxMDYzYTJmZmI1YjUxNyIsIm5iZiI6MTcyOTM0ODM1Mi4xODI2NjYsInN1YiI6IjY3MTNjMTE1OTlmMjJmMzI2YWFkMTQ5NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Mv_OHZYBdIJq0uFSYNvdljUV3vPckQGdVJcHD76lz3c'
-  }
-};
-
-fetch(url, options)
-  .then(res => res.json())
-  .then(json => console.log(json))
-  .catch(err => console.error('error:' + err));
+const callAPI = async (show_id) => {
+    let request = await fetch(`https://api.themoviedb.org/3/tv/${show_id}`, {
+        method: 'GET',
+        headers: {
+            accept: 'application.json',
+            Authorization: `Bearer ${VITE_API_ACCESS_TOKEN}`
+        }
+    })
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.error('error:' + err));
+}
+console.log(await callAPI(456));
